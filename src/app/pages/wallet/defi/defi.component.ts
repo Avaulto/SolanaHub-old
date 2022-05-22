2329faaa82d979bffd539953e5ffecd80f53cf00
amir cohen <amircohen@amirs-MacBook-Pro.local>
2022-05-22 21:56:08 +0300

    defi add on

--- src/app/pages/wallet/defi/defi.component.ts
+++ src/app/pages/wallet/defi/defi.component.ts
@@ -9,6 +9,19 @@ export class DefiComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
-
+  ngOnInit() { }
+  public defiApps = [
+    {
+      name: 'Jupiter',
+      desc: 'Swap Infrastructure for Solana',
+      img: 'https://jup.ag/svg/jupiter-logo.svg',
+      link: '#'
+    },
+    {
+      name: 'magic eden',
+      desc: 'Home to the next generation of digital creators',
+      img: 'https://play-lh.googleusercontent.com/Z1Bws4X7ZpDWT5qaEO9D2LO3Ts9vvyUQNJFtnHkFrmNBL6JaxjDvHjJtlPGSyELdlAs',
+      link: '#'
+    }
+  ]
 }
