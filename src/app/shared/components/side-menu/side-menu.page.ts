34295866bbe82e870bf5e53d38e2a6f6e4b36e6e
amir cohen <amircohen@amirs-MacBook-Pro.local>
2022-09-13 12:35:48 +0300

    add tnc

--- src/app/shared/components/side-menu/side-menu.page.ts
+++ src/app/shared/components/side-menu/side-menu.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { faKickstarterK } from '@fortawesome/free-brands-svg-icons';
 import { faArrowRightArrowLeft, faBox, faBoxOpen, faCoins, faDroplet, faHandHoldingDroplet, faHome, faImage, faPalette, faPlus, faScrewdriverWrench, faSeedling, faShieldHeart, faSwimmingPool, faWallet } from '@fortawesome/free-solid-svg-icons';
-import { MenuController } from '@ionic/angular';
+import { IonPopover, MenuController } from '@ionic/angular';
 import { pages } from '../../helpers/menu';
 
 
@@ -11,7 +11,7 @@ import { pages } from '../../helpers/menu';
   styleUrls: ['./side-menu.page.scss'],
 })
 export class SideMenuPage implements OnInit {
-
+  @ViewChild('popover') popover:IonPopover;
   public profileIcon = faScrewdriverWrench;
   public boxOpen = faBoxOpen
   constructor(private menu: MenuController,
@@ -28,14 +28,19 @@ export class SideMenuPage implements OnInit {
     this.menu.open('end');
   }
 
-  currencies: string[] = ['BTC', 'USD'];
   pages = pages
 
-  addNewWallet() {
-  }
   ngOnInit() {
   }
-  selectCurrency(ev) {
+  isTncOpen = false;
+
+  presentTncPopover(e?: Event) {
+    this.popover.event = null;
+    this.popover.cssClass = 'tncPopup'
+    this.isTncOpen = true;
   }
 
+  openTNC(){
+
+  }
 }
