c5bfe362a0a7a9ffd959770531c5644e364004b4
amir cohen <amircohen@amirs-MacBook-Pro.local>
2022-10-02 01:43:49 +0300

    remove wallet login require

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,9 @@ import {
   SolflareWalletAdapter,
   SolletExtensionWalletAdapter,
   SolletWalletAdapter,
-  SolongWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
-import { Platform } from '@ionic/angular';
-import { DappioNavService } from './services/dappio-nav.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -25,19 +21,13 @@ export class AppComponent {
   constructor(
     public router:Router,
     private _connectionStore: ConnectionStore,
-    private _walletStore: WalletStore,
-    private dappioNav: DappioNavService
+    private _walletStore: WalletStore
   ) { }
   async ngOnInit(): Promise<void> {
-    // this.dappioNav.fetchRayPools()
-    // this.userService.populate();
-    // mb-avaulto-cc28.mainnet.rpcpool.com/f72a3ed2-f282-4523-95a0-d4acfcd40f4d
-    // 'https://dawn-chaotic-butterfly.solana-mainnet.discover.quiknode.pro/e3aa076aff3f4a6b638c3599348ab5f56fde2a12/'
     connectionConfigProviderFactory({
       commitment: "confirmed",
     })
     this._connectionStore.setEndpoint(environment.solanaCluster) ;
-    // this._connectionStore.setEndpoint('https://dawn-chaotic-butterfly.solana-mainnet.discover.quiknode.pro/e3aa076aff3f4a6b638c3599348ab5f56fde2a12');
     this._walletStore.setAdapters([
       new PhantomWalletAdapter(),
       new ExodusWalletAdapter(),
