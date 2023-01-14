cb2a9a42961ff62c792fa0c78bc736b0a78d60c5
amir cohen <inbox@avaulto.com>
2023-01-14 23:55:54 +0200

    add type & refactor labels

--- src/app/shared/components/label-layout/label-layout.component.ts
+++ src/app/shared/components/label-layout/label-layout.component.ts
@@ -2,8 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-label-layout',
-  template: `  <ion-row style="margin-bottom:8px" class="heading-6 ion-justify-content-between">
-                <div id="">{{layoutConfig.title}} <app-icon-tooltip *ngIf="layoutConfig.toolTip && !layoutConfig.balance" [tip]="layoutConfig.toolTip"></app-icon-tooltip></div>
+  template: `  <ion-row style="margin-bottom:8px" class="heading-6 ion-align-items-center ion-justify-content-between">
+                <div id="label"><span>{{layoutConfig.title}} </span><app-icon-tooltip *ngIf="layoutConfig.toolTip && !layoutConfig.balance" [tip]="layoutConfig.toolTip"></app-icon-tooltip></div>
                 <div id="balance" *ngIf="layoutConfig.balance">
                     Balance: {{layoutConfig.balance}}
                     <app-icon-tooltip *ngIf="layoutConfig.toolTip" [tip]="layoutConfig.toolTip"></app-icon-tooltip>
