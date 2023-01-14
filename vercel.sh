3763b42faeb864a3632bef57412a741e861d1fe2
amir cohen <inbox@avaulto.com>
2023-01-14 23:05:50 +0200

    update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-if [[ $VERCEL_GIT_COMMIT_REF == "main"  ]] ; then 
+if [[ $VERCEL_GIT_COMMIT_REF == "master"  ]] ; then 
   echo "This is our main branch"
   npm run build:main
 else 
