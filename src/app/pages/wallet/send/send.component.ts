c17a707d1c345faea0e9963eb8589296d8b8724a
amir cohen <inbox@avaulto.com>
2023-02-05 13:37:09 +0200

    add floor price & Total value to nft gallery

--- src/app/pages/wallet/send/send.component.ts
+++ src/app/pages/wallet/send/send.component.ts
@@ -2,8 +2,7 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
-import { Elusiv, TokenType } from "elusiv-sdk";
-import { firstValueFrom, observable, Observable, Subscriber } from 'rxjs';
+// import { Elusiv, TokenType } from "elusiv-sdk";
 import { Asset } from 'src/app/models';
 import { LoaderService, UtilsService } from 'src/app/services';
 import { SolanaUtilsService } from 'src/app/services/solana-utils.service';
@@ -66,10 +65,10 @@ export class SendComponent implements OnInit {
     const walletOwnerPublicKey = new PublicKey(this.wallet.address)
     // try {
       if (privateTx) {
-        const randomInput = Math.random().toString(36).slice(2, 7);
-        const seed = (new TextEncoder()).encode(Elusiv.hashPw(randomInput)) as Buffer;
-        const signedSeed = await firstValueFrom(this._wallet.signMessage(seed));
-        const elusiv = await Elusiv.getElusivInstance(signedSeed, walletOwnerPublicKey, this._solanaUtilsService.connection); // this line return err
+        // const randomInput = Math.random().toString(36).slice(2, 7);
+        // const seed = (new TextEncoder()).encode(Elusiv.hashPw(randomInput)) as Buffer;
+        // const signedSeed = await firstValueFrom(this._wallet.signMessage(seed));
+        // const elusiv = await Elusiv.getElusivInstance(signedSeed, walletOwnerPublicKey, this._solanaUtilsService.connection); // this line return err
         // console.log(elusiv)
         // // Top up our private balance with 1 SOL
         // const topupTxData = await elusiv.buildTopUpTx(amount * LAMPORTS_PER_SOL, 'LAMPORTS');
