cf1aa9865e42b9bedb2328d45a2f9f77a24c5875
amir cohen <inbox@avaulto.com>
2023-02-06 09:16:35 +0200

    pre ng15

--- src/app/pages/wallet/send/send.component.ts
+++ src/app/pages/wallet/send/send.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import { firstValueFrom } from 'rxjs';
 // import { Elusiv, TokenType } from "elusiv-sdk";
 import { Asset } from 'src/app/models';
 import { LoaderService, UtilsService } from 'src/app/services';
@@ -78,7 +79,7 @@ export class SendComponent implements OnInit {
 
         // const sendPrivatlytX = await elusiv.buildSendTx(amount * LAMPORTS_PER_SOL, targetPk, 'LAMPORTS');
         // const sendRes = await elusiv.sendElusivTx(sendPrivatlytX);
-        // Send SOL, privately 😎
+        // // Send SOL, privately 😎
         // const res = await this._txInterceptService.sendTx([topupTxData.tx, ], walletOwnerPublicKey)
 
       } else {
