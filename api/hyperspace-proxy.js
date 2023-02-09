52591498584409e32c68d67fee245abab3079c72
AC-devon <inbox@avaulto.com>
2023-02-09 21:45:46 +0200

    ng15 & frakt phase 1

--- api/hyperspace-proxy.js
+++ api/hyperspace-proxy.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 export default async function HSproxy(request, response) {
     const {  endpoint, queryParam } = request.query;
+    const {body} = request
     const queryParamDecode = decodeURIComponent(queryParam);
     const url = `https://beta.api.solanalysis.com/rest/${endpoint}?${queryParamDecode}`;
     const settings = {
@@ -9,7 +10,7 @@ export default async function HSproxy(request, response) {
             Authorization:  process.env.hyperspaceToken
         },
     }
-    const res = await fetch(url, settings);
+    const res = await fetch(url, settings,body);
     const data = await res.json();
     return response.status(200).json( data );
 }
