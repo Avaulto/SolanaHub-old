144e08e03d69928e73699690809f0b5d576dbc9d
AC-devon <inbox@avaulto.com>
2023-05-12 13:32:14 +0300

    test db 2

--- api/db/addVote.js
+++ api/db/addVote.js
@@ -10,40 +10,34 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+
 export default async function votes(request, response) {
   const { endpoint } = request.query;
   const { body } = request
-  // const queryParamDecode = decodeURIComponent(queryParam);
-  await client.db.collection('votes').insertOne({
-    uuid:"234534645",
-    date: new Date(),
-    category: "integration",
-    title: "orca integration",
-    desc: "This proposal will - Ratify the Constitution to be used by Marinade DAO - Ratify the Code of Conduct to be used by Marinade DAO - Confirm phase one of the migration to SPL governance, where multisigs and team powers are moved to Realms while MNDE voting stays on Tribeca until phase two.",
-    ownerPK: "JPQmr9p2RF3X5TuBXxn6AGcEfcsHp4ehcmzE5Ys7pZD",
-    for: 100,
-    against: 15,
-    status: "vote",
-  });
   try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // const queryParamDecode = decodeURIComponent(queryParam);
+    await client.db.collection('votes').insertOne({
+      uuid: "234534645",
+      date: new Date(),
+      category: "integration",
+      title: "orca integration",
+      desc: "This proposal will - Ratify the Constitution to be used by Marinade DAO - Ratify the Code of Conduct to be used by Marinade DAO - Confirm phase one of the migration to SPL governance, where multisigs and team powers are moved to Realms while MNDE voting stays on Tribeca until phase two.",
+      ownerPK: "JPQmr9p2RF3X5TuBXxn6AGcEfcsHp4ehcmzE5Ys7pZD",
+      for: 100,
+      against: 15,
+      status: "vote",
+    });
     // const res = await fetch(url, settings, JSON.stringify(body));
-    const data = await res.json({message:'vote added'});
+    const data = await res.json({ message: 'vote added' });
     return response.status(200).json(data);
   } catch (error) {
     return response.status(500).json(error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 
 }
