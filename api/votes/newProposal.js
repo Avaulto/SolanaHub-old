f314d59587754cde1b5173d5123d22c73e10edf4
AC-devon <inbox@avaulto.com>
2023-05-12 22:48:21 +0300

    vote merge

--- api/votes/newProposal.js
+++ api/votes/newProposal.js
@@ -14,15 +14,15 @@ const client = new MongoClient(uri, {
 
 export default async function votes(request, response) {
   const { endpoint } = request.query;
-  const { body } = request
+  const { proposal } = request.body
   try {
-    console.dir(request.body)
+    console.dir(proposal)
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // const queryParamDecode = decodeURIComponent(queryParam);
     const db = await client.db("CDv1")
     const collection = db.collection('votes')
-    const newProposal = {...body.proposal,for:1,agains:0, date: new Date().now(), signers:[] }
+    const newProposal = {...proposal,for:1,agains:0, date: new Date().now(), signers:[] }
     const newItem = await collection.insertOne(newProposal);
     // const res = await fetch(url, settings, JSON.stringify(body));
     // const data = await res.json({ message: 'vote added' });
