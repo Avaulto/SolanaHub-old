1d678ed2f5e2a90906de13815082d190dff39f36
Your Name <you@example.com>
2023-07-15 15:42:30 -0400

    hard coding prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     config: '2LecshUwdy9xi7meFgHtFJQNSKk4KdTrcpvaB56dP2NQ',
     poolAPI: 'https://api.mainnet.orca.so/v1/whirlpool/list'
   },
-  solanaCluster: 'https://mb-avaulto-cc28.mainnet.rpcpool.com',
+  solanaCluster: 'https://rpc.helius.xyz/?api-key=a837695c-41e3-42f4-82a4-abd8db35a792',
   serverlessAPI: 'https://beta.compact-defi.xyz',
-  platformFeeCollector:'81QNHLve6e9N2fYNoLUnf6tfHWV8Uq4qWZkkuZ8sAfU1'
+  platformFeeCollector:'6X3oVE5Hq923M2UEJregoA7zLxuc2jXcJJegpy24pb2T'
 };
