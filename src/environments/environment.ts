0869860bdad6fc23b0f1d409f9152ac39154f011
Jarett Dunn <jarettrsdunn+git@gmail.com>
2023-07-17 11:01:12 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     config: '2LecshUwdy9xi7meFgHtFJQNSKk4KdTrcpvaB56dP2NQ',
     poolAPI: 'https://api.mainnet.orca.so/v1/whirlpool/list'
   },
-  solanaCluster: 'https://rpc.helius.xyz/?api-key=a837695c-41e3-42f4-82a4-abd8db35a792',
+  solanaCluster: 'https://rpc.helius.xyz/?api-key=e958f7ae-f1c3-432b-91fb-c1e144e32ecc',
   serverlessAPI: 'https://beta.compact-defi.xyz',
   platformFeeCollector:'6X3oVE5Hq923M2UEJregoA7zLxuc2jXcJJegpy24pb2T'
 };
