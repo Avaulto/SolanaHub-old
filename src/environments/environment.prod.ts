3c3c507031d380d7b926b7893d1e1e0200bdc05c
AC-devon <inbox@avaulto.com>
2023-08-18 12:57:02 +0300

    r

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   solanaEnv: 'mainnet-beta',
-  solanaCluster: 'https://mb-avaulto-cc28.mainnet.rpcpool.com/190dce2e-b99d-475e-8e0f-3207a5c97e8b',
-    serverlessAPI: 'https://beta.compact-defi.xyz',
+  solanaCluster: 'https://mb-avaulto-cc28.mainnet.rpcpool.com',
+  serverlessAPI: 'https://beta.compact-defi.xyz',
   platformFeeCollector:'81QNHLve6e9N2fYNoLUnf6tfHWV8Uq4qWZkkuZ8sAfU1'
 };
