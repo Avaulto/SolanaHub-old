73c4f660e34152dad5d9f8802073dafa34aaceed
AC-devon <inbox@avaulto.com>
2023-08-28 19:08:45 +0300

    update cron job

--- api/loyalty-points/get-validator-bribe.js
+++ api/loyalty-points/get-validator-bribe.js
@@ -14,9 +14,9 @@ export default async function getValidatorBribe(request, response) {
     try {
         await client.connect();
         // Sort in descending order by timestamp field
-        // const sortOptions = { timestampField: 1 };
+        const sortOptions = { timestampField: -1 };
 
-        const getValidatorBribe = await client.db("CDv1").collection("validator-bribe").find().sort({_id:-1}).limit(1);
+        const getValidatorBribe = await client.db("CDv1").collection("validator-bribe").find().sort( [['_id', -1]]).limit(1)
         const latest = getValidatorBribe.validatorBribeData
         return response.status(200).json(latest);
     } catch (error) {
